0594e863eabf866050b7b0273c7c01c3ccb54689
Sergey Chv <serge.techno@gmail.com>
2024-05-30 23:59:40 +0300

    work with nav menu component - pqrt 2

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -8,14 +8,19 @@ function Nav() {
       <Disclosure as="nav">
         {({ open }) => (
             <>
-                <div className="flex">
+                <div className="flex h-[15vh] items-center max-w-7xl px-8 justify-between">
+                    <div className="flex">
                     <a href="#">
                         <img src={Kobodrop} alt="logo pic" />
                     </a>
-                    <div className="flex">
-                        <div>ButtonLink</div>
-                        <div>ButtonLink</div>
-                        <div>ButtonLink</div>
+                    <div className="flex space-x-4 items-center ml-4">
+                        <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#features">Features</a>
+                        <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#partners">Partners</a>
+                        <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#reviews">Reviews</a>
+                    </div>
+                    <a href="https://play.google.com" target="_blank" 
+                    className="bg-gray-800 text-white rounded-xl px-5 py-3 hover:bg-gray-900 active:bg-gray-700">Install Kobodrop</a>
+                    <Disclosure.Button></Disclosure.Button>
                     </div>
                 </div>
             </>
