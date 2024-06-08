fa324fb170d55d0e4ab4f79540ebb3df14ea932d
Sergey Chv <serge.techno@gmail.com>
2024-06-08 21:10:26 +0300

    fixed project

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -13,16 +13,25 @@ function Nav() {
                     <a href="#">
                         <img src={Kobodrop} alt="logo pic" />
                     </a>
-                    <div className="flex space-x-4 items-center ml-4">
+                    <div className="space-x-4 items-center ml-4 hidden sm:flex">
                         <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#features">Features</a>
                         <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#partners">Partners</a>
                         <a className="text-gray-500 hover:text-gray-900 active:text-gray-400" href="#reviews">Reviews</a>
                     </div>
-                    <a href="https://play.google.com" target="_blank" 
-                    className="bg-gray-800 text-white rounded-xl px-5 py-3 hover:bg-gray-900 active:bg-gray-700">Install Kobodrop</a>
-                    <Disclosure.Button></Disclosure.Button>
                     </div>
+                    <a href="https://play.google.com" target="_blank" 
+                    className="active:bg hidden rounded-xl bg-gray-800 text-white px-5 py-3 hover:bg-gray-900 active:bg-gray-700 sm:flex">Install Kobodrop</a>
+                    <Disclosure.Button className="sm:hidden rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white ">
+                        { open ? (
+                            <XMarkIcon className="block h-6 w-6" />
+                        ) : <Bars3Icon className="block h-6 w-6"  />} 
+                    </Disclosure.Button> 
                 </div>
+                <Disclosure.Panel className="space-y-1">
+                    <Disclosure.Button>
+                        
+                    </Disclosure.Button>
+                </Disclosure.Panel>
             </>
         )}
       </Disclosure>
