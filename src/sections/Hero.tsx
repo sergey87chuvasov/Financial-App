8f68992aaaf366b11353745dd6d80d7bf197407c
Sergey87 <serge.techno@gmail.com>
2024-06-12 23:18:11 +0300

    added StoreLink component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,6 @@
 import AppleLogo from "../assets/logos/app_store.svg";
+import GooglePlay from "../assets/logos/google_play.svg";
+import StoreLink from "../common/StoreLink";
 
 function Hero() {
   return (
@@ -11,16 +13,22 @@ function Hero() {
         </p>
       </div>
       <div className="mt-10 hidden justify-center space-x-2 sm:flex md:justify-normal">
-        <a
+        <StoreLink
           href="https://www.apple.com/app-store"
+          upperText="Download on the"
+          lowerText="App Store"
+          logo={AppleLogo}
+          target="_blank"
           className="hover: flex gap-3 rounded-lg bg-zinc-900 bg-zinc-950 px-4 py-3 text-white hover:bg-zinc-950 active:bg-zinc-800"
-        >
-          <img src={AppleLogo} alt="logo pic" className="w-5" />
-          <div>
-            <p className="text-xs">Download on the</p>
-            <p>App Store</p>
-          </div>
-        </a>
+        />
+        <StoreLink
+          href="https://play.google.com"
+          upperText="Get it on"
+          lowerText="Google Play"
+          logo={GooglePlay}
+          target="_blank"
+          className="hover: flex gap-3 rounded-lg bg-zinc-900 bg-zinc-950 px-4 py-3 text-white hover:bg-zinc-950 active:bg-zinc-800"
+        />
       </div>
     </section>
   );
